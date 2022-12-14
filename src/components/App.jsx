bd03104574291741e3add513009da75a57b547cb
guilhiz <guilhermevilelasobral@gmail.com>
2022-12-14 11:51:53 -0300

    commit initial

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import 'reset-css';
+import GlobalStyle from '../styles/GlobalStyle';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+     <div>
+     <h1>OIIIII</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
